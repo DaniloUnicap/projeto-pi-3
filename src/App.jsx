14dc124c0a547badb90aaf43b45e158f0cd7ffb3
daniloUnicap <danilo.00000850307@unicap.br>
2024-05-20 08:34:29 -0300

    Gerenciamento de estado global com useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { useState } from 'react'
-
+import React from 'react';
 import { Inicio } from './pages/Inicio'
 import { Trilhas } from './pages/Trilhas'
 import { Nav } from '../src/components/Nav';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 import { Cursos } from './pages/Cursos';
 import { Aulas } from './pages/Aulas';
 import { Footer } from './components/Footer';
-// import { bg } from '../src/assets/bg.jpeg'
-//Teste
+import { useContext } from 'react';
+import { ThemeContext } from './context/ThemeContext';
+// import { NotFound } from './components/NotFound';
 
 
 function App() {
 
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <div className='text-white bg-[#1C1A1D] font-[Poppins]'>
+    <div className={` font-[Poppins] ${theme === "ligth" ? "bg-[#f4f4f5] text-black" : "text-white"}`}>
       <Nav />
 
       <main className='max-w-[1920px] mx-auto'>
@@ -25,11 +27,12 @@ function App() {
           <Route path="/cursos/:categoria" element={<Cursos />} />
           <Route path="/cursos/:categoria/:curso" element={<Aulas />} />
           <Route path="/cursos/:categoria/:curso/aula/:id" element={<Aulas />} />
+          {/* <Route path='*' element={<NotFound />} /> */}
         </Routes>
 
       </main>
 
-      <Footer/>
+      <Footer />
 
     </div>
   )
--- src/pages/Cursos.jsx
+++ src/pages/Cursos.jsx
@@ -2,9 +2,11 @@ import { useParams } from 'react-router-dom'
 import { cursosFront, cursosBack, cursosFerramentasAgeis, cursosRedes } from '../data/cursos'
 import "../App.css"
 import { Link } from "react-router-dom"
+import { useContext } from "react"
+import { ThemeContext } from "../context/ThemeContext"
 
 export const Cursos = () => {
-
+    const { theme } = useContext(ThemeContext);
     const { categoria } = useParams();
     let cursos;
     if (categoria === 'frontend') {
@@ -23,7 +25,7 @@ export const Cursos = () => {
             <div className=' grid grid-cols-1 gap-y-8  lg:grid-cols-3'>
                 {cursos.map(item => (
                     <Link to={`/cursos/${categoria}/${item.curso}`} key={item.titulo}>
-                    <div className='bg-[#242125] w-[260px] mx-auto rounded-lg flex items-center py-2 px-4 transition-transform transform hover:scale-110 xl:w-[320px]'>
+                    <div className={`${theme === "ligth" ? "bg-gray-300 text-black" : "bg-[#242125] text-white"} w-[260px] mx-auto rounded-lg flex items-center py-2 px-4 transition-transform transform hover:scale-110 xl:w-[320px`}>
                       <img src={item.imagem} alt={item.titulo} className='w-24' />
                       <h2 className='font-bold w-full text-center'>{item.curso}</h2>
                     </div>
