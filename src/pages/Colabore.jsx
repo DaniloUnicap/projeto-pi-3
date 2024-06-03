e46b8c2dc3ff8a113028d7276db04fd121c4adf2
giovannaaraujo1 <giovannaaraujoslv@gmail.com>
2024-06-03 20:13:42 -0300

    Ajustes no layout da página de colaboração e adição de imagem

--- src/pages/Colabore.jsx
+++ src/pages/Colabore.jsx
@@ -1,5 +1,6 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
 import PixQRCodeGenerator from '../components/PixQRCodeGenerator'
+import hero from '../assets/colabore/hero.png'
 
 export const Colabore = () => {
 
@@ -11,24 +12,30 @@ export const Colabore = () => {
     ]
 
     return (
-        <div className='px-4 md:px-16'>
-            <h1 className="text-4xl font-semibold text-center mt-8 mb-12">Colabore com o nosso projeto</h1>
+        <div className='px-4 md:px-48'>
+            
 
-            <section className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+            <section className="grid grid-cols-1 gap-16 lg:grid-cols-2 lg:mt-12 lg:gap-4 lg:justify-center">
                 <div>
-                    <p className="px-4 leading-7 mx-auto">
-                        A BirdTech nasceu a partir do desejo de incentivar os estudos na área tech, com isso buscamos promover gratuitamente conteúdos filtrados para pessoas que não possuem condições financeiras para adquirir materiais pagos. Para continuarmos com nosso trabalho, é necessário que haja uma manutenção constante, para mantermos a comunidade cada vez mais atualizada. Portanto, se você foi ajudado em algum momento por nossa plataforma e tiver condições para ajudar novos estudantes, aceitamos doações apartir de R$2,00. Cada contribuição, por menor que seja, faz a diferença e nos ajuda a continuar oferecendo suporte a novos estudantes.
-                        Caso não possua condições, nos ajude compartilhando e espalhando nossa plataforma para novas pessoas, você poderá ajudar novos ingressantes da área TECH!
-                        Juntos, podemos construir uma comunidade mais inclusiva e capacitada na área de tecnologia. Obrigado por considerar apoiar nosso projeto e por ser parte dessa jornada conosco!
+                <h1 className="text-4xl font-semibold text-center mb-12 lg:text-start">Colabore com o nosso projeto</h1>
+                    <p className="text-justify lg:text-start">
+                        A BirdTech nasceu a partir do desejo de incentivar os estudos na área tech, com isso buscamos promover gratuitamente conteúdos filtrados para pessoas que não possuem condições financeiras para adquirir materiais pagos. Para continuarmos com nosso trabalho, é necessário que haja uma manutenção constante, para mantermos a comunidade cada vez mais atualizada. Portanto, se você foi ajudado em algum momento por nossa plataforma e tiver condições para ajudar novos estudantes, aceitamos doações a partir de R$2,00.
                     </p>
                 </div>
 
-                <div className="w-max-[400px] h-[400px]  mx-auto">
+                <div className=''>
+                    <img src={hero} alt="" className='mx-auto' />
+                </div>
+            </section>
+
+            <div className='mt-20'>
+                <div className="max-w-[400px]  mx-auto">
                     <PixQRCodeGenerator>
 
                     </PixQRCodeGenerator>
                 </div>
-            </section>
+            </div>
+                
 
             <section>
                 <h2 className="text-2xl font-semibold text-center my-16 ">Algum título aqui</h2>
