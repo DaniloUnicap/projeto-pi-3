ebf2b1271bff2444002e726432105ddf54a45151
tonigds1 <a.guedes.cn@gmail.com>
2024-06-05 15:25:50 -0300

    Add questões

--- src/components/TesteModulo.jsx
+++ src/components/TesteModulo.jsx
@@ -1,5 +1,35 @@
 import React, { useState } from 'react';
 
+const questions = [
+    {
+      question: "1. Qual é a função da tag <head> em um documento HTML?",
+      options: ["Contém metadados sobre o documento", "Define o corpo do documento", "Cria um parágrafo", "Adiciona um link externo"],
+      answer: "a",
+    },
+    {
+      question: "2. Qual é o propósito da declaração <!DOCTYPE html>?",
+      options: ["Define a linguagem do documento", "Define a versão do HTML", "Adiciona estilos ao documento", "Cria um script"],
+      answer: "b",
+    },
+    {
+      question: "3. O que faz a tag <meta name='viewport' content='width=device-width, initial-scale=1.0'>?",
+      options: ["Define a largura e o nível de zoom da página para dispositivos móveis", "Adiciona um título à página", "Define a codificação de caracteres", "Cria uma lista não ordenada"],
+      answer: "a",
+    },
+    {
+      question: "4. Qual atributo é usado para aplicar um estilo único a um elemento HTML?",
+      options: ["class", "style", "id", "src"],
+      answer: "c",
+    },
+    {
+      question: "5. Quantos níveis de cabeçalhos existem em HTML?",
+      options: ["3", "5", "6", "8"],
+      answer: "c",
+    },
+    
+      
+  ];
+
 
 
 export const  TesteModulo = () => {
